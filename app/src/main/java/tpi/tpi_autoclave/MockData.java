bf3136b3052c64c7430a973a48e5297b48b69d56
Litier <Litier.4@gmail.com>
2015-07-21 00:29:05 -0300

    cree la pantalla de inicio

--- app/src/main/java/tpi/tpi_autoclave/MockData.java
+++ app/src/main/java/tpi/tpi_autoclave/MockData.java
@@ -5,15 +5,13 @@ import java.util.Random;
 
 public class MockData {
 
-	// x is the day number, 0, 1, 2, 3
-	public static Point getDataFromReceiver(int x, int y)
-	{
-		return new Point(x, y);
-	}
-	
-	private static int generateRandomData()
-	{
-		Random random = new Random();
-		return random.nextInt(40);
-	}
+    // x is the day number, 0, 1, 2, 3
+    public static Point getDataFromReceiver(int x, int y) {
+        return new Point(x, y);
+    }
+
+    private static int generateRandomData() {
+        Random random = new Random();
+        return random.nextInt(40);
+    }
 }
--- app/src/main/java/tpi/tpi_autoclave/Point.java
+++ app/src/main/java/tpi/tpi_autoclave/Point.java
@@ -2,22 +2,21 @@ package tpi.tpi_autoclave;
 
 
 public class Point {
-	
-	private int x;
-	private int y;
-	
-	public Point( int x, int y)
-	{
-		this.x = x;
-		this.y = y;
-	}
-	
-	public int getX() {
-		return x;
-	}
 
-	public int getY() {
-		return y;
-	}
-	
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
 }
